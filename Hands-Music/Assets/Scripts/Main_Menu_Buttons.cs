4a5ee135acabd284e8df562708f375a407a063d4
Pierre-LS <pierre00@mit.edu>
2023-11-18 00:03:03 -0500

    Changed the Aesthetic of the Main Menu

---
+++ Hands-Music/Assets/Scripts/Main_Menu_Buttons.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Button : MonoBehaviour
+{
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+
+    GameObject presser;
+    bool isPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            presser = other.gameObject;
+            onPress.Invoke();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject ==  presser)
+        {
+            onRelease.Invoke();
+            isPressed=false;
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Game closed");
+    }
+    public void ExploreButton()
+    {
+        SceneManager.LoadSceneAsync("Simulation");
+    }
+
+    public void SettingsButton()
+    {
+        Debug.Log("To Do");
+    }
+}
