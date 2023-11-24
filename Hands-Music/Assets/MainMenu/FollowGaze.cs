3762540df1a9a7cf5168fa508f2516e371a8441e
Pierre-LS <pierre00@mit.edu>
2023-11-24 15:17:09 -0500

    Added a gaze following feature for the menu and upgraded the visuals

--- Hands-Music/Assets/MainMenu/FollowGaze.cs
+++ Hands-Music/Assets/MainMenu/FollowGaze.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class FollowGaze : MonoBehaviour
 {
-    public Transform Camera2Follow;
+    public Camera Camera2Follow;
 
-    private float SmoothSpeed = 0.125f;
-    private Vector3 offset = new Vector3(-0.3714488f, 1.045249f, 0.06647553f);
+    private float SmoothSpeed = 0.3f;
+    public float distanceToHead = 0.3f;
+
     private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     private void Update()
     {
-        Vector3 desiredPosition = Camera2Follow.position + offset;
-        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, SmoothSpeed);
-        transform.position = smoothedPosition;
+        // make the UI always face towards the camera
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, Camera2Follow.transform.eulerAngles.y, transform.eulerAngles.z);
 
-        transform.LookAt(Camera2Follow);
+        var currentPos = transform.position;
+
+        // target is in the same direction but offsetRadius from the center
+        var targetPosition = new Vector3(Camera2Follow.transform.position.x + distanceToHead*Mathf.Sin(Mathf.Deg2Rad * Camera2Follow.transform.eulerAngles.y), Camera2Follow.transform.position.y - 0.1f, Camera2Follow.transform.position.z + distanceToHead * Mathf.Cos(Mathf.Deg2Rad * Camera2Follow.transform.eulerAngles.y));
+
+        // finally interpolate towards this position
+        transform.position = Vector3.SmoothDamp(currentPos, targetPosition, ref velocity, SmoothSpeed);
+
+        //Vector3 desiredPosition = Camera2Follow.position + offset;
+        //Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, SmoothSpeed);
+        //transform.position = smoothedPosition;
+
+        //transform.LookAt(Camera2Follow);
     }
 }
