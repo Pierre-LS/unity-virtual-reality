a769b758db11f465548a7ff3a2200283ec4c40f9
Pierre-LS <pierre00@mit.edu>
2023-11-27 19:31:23 -0500

    Added Level Selection in Main Menu

--- Hands-Music/Assets/Scripts/Main_Menu_Buttons.cs
+++ Hands-Music/Assets/Scripts/Main_Menu_Buttons.cs
@@ -10,12 +10,25 @@ public class Main_Menu_Button : MonoBehaviour
     public UnityEvent onPress;
     public UnityEvent onRelease;
 
-    GameObject presser;
-    bool isPressed;
+    private Transform menu_buttons;
+    private GameObject explore_button;
+    private GameObject exit_button;
+    private GameObject forest_button;
+    private GameObject ocean_button;
+    private GameObject back_button;
+
+    private GameObject presser;
+    private bool isPressed;
 
     // Start is called before the first frame update
     void Start()
     {
+        menu_buttons = transform.parent.gameObject.transform.parent;
+        explore_button = menu_buttons.Find("Explore Button").gameObject;
+        exit_button = menu_buttons.Find("Exit Button").gameObject;
+        forest_button = menu_buttons.Find("Forest Button").gameObject;
+        ocean_button = menu_buttons.Find("Ocean Button").gameObject;
+        back_button = menu_buttons.Find("Back Button").gameObject;
         isPressed = false;
     }
 
@@ -31,10 +44,10 @@ public class Main_Menu_Button : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject ==  presser)
+        if (other.gameObject == presser)
         {
             onRelease.Invoke();
-            isPressed=false;
+            isPressed = false;
         }
     }
 
@@ -45,7 +58,30 @@ public class Main_Menu_Button : MonoBehaviour
     }
     public void ExploreButton()
     {
+        explore_button.SetActive(false);
+        exit_button.SetActive(false);
+        forest_button.SetActive(true);
+        ocean_button.SetActive(true);
+        back_button.SetActive(true);
+    }
+    
+    public void ForestButton()
+    {
         SceneManager.LoadSceneAsync("Simulation");
+    }
+
+    public void OceanButton()
+    {
+        SceneManager.LoadSceneAsync("Simulation");
+    }
+
+    public void BackButton()
+    {
+        explore_button.SetActive(true);
+        exit_button.SetActive(true);
+        forest_button.SetActive(false);
+        ocean_button.SetActive(false);
+        back_button.SetActive(false);
     }
 
     public void MainMenuButton()
